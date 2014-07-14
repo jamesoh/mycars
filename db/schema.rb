207a5d226ef5787bccc22872c6e34b50e3a98364
jamesoh <joh@truecar.com>
2014-07-14 13:57:48 -0700

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714175633) do
+ActiveRecord::Schema.define(version: 20140714204337) do
 
   create_table "cars", force: true do |t|
     t.string   "vin"
